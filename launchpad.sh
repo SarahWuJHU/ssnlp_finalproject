25236718a0e78973098f6b8775f5a6c3ddcb663a
Sarah Wu <swu82@jh.edu>
2023-05-02 14:54:15 -0400

    more gpu resource per task

--- launchpad.sh
+++ launchpad.sh
@@ -6,8 +6,8 @@
 #SBATCH --reservation=MIG
 #SBATCH --qos=qos_mig_class
 #SBATCH --gres=gpu:1
-#SBATCH --nodes=2
-#SBATCH --ntasks-per-node=1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
 #SBATCH --time=2:00:00
 #SBATCH --job-name="Final Project"
 
